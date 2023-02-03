ff3053fcc9231e111af4a9158a4366c72374a78a
medranomiler <67513942+medranomiler@users.noreply.github.com>
2023-02-02 22:02:26 -0600

    Add files via upload

---
+++ public/assets/js/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path');
+const noteData = require('../../../db/db.json');
+
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../../notes.html'))
+  
+);
+
+app.get('/notes', (req, res) => res.json(noteData));
+
+app.get('*', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../../index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
