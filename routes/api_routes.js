b3768181ae50d346a972925d1f10e823da95cf21
Medranomiler1 <medranomiler@gmail.com>
2023-02-03 20:16:29 -0600

    added status codes and formatted promises using async/ await and try/catch

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -4,19 +4,51 @@ const utils = require("../helpers/fs-utilities");
 
 
 router.get('/notes', async (req, res) => {
-    const data = await utils.readFromFile('./db/db.json');
-    res.json(JSON.parse(data));
+  try {
+      const data = await utils.readFromFile('./db/db.json');
+      res.status(200).json(JSON.parse(data));
+  } catch(err) {
+      console.error(err);
+      res.status(400).json({ error: 'Unable to load notes. Please try again.' });
+  }
 });
 
+router.get("/notes/:id", async (req, res) => {
+  try {
+    const notes = await utils.readFromFile("./db/db.json");
+    const parsedNotes = JSON.parse(notes);
+    const note = parsedNotes.find(n => n.id === req.params.id);
+    if (!note) {
+      res.status(404).json({ error: "Note not found" });
+    } else {
+      res.status(200).json(note);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong while retrieving the note" });
+  }
+});
+
+
 router.post("/notes", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.text) {
+      return res.status(400).json({ error: "our note was not able to be posted. Please try again. Title and text are required fields" });
+    }
     let newNote = {
       title: req.body.title,
       text: req.body.text,
       id: uuid()
     };
     await utils.readAndAppend(newNote, "./db/db.json");
-  
-    res.json("Success");
-  });
+    res.status(201).json("Success");
+  }
+  catch(err) {
+    console.error(err);
+    res.status(500).json({ error: "Your note was not able to be posted. Please try again." });
+  }
+});
+
+
 
 module.exports = router;
